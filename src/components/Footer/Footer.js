73b9d9a82af16f4535dad8976647c12ce20c7ec3
phu3112004 <22521105@gm.uit.edu.vn>
2024-10-13 10:24:37 +0700

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div>
+      <h1>Day la footer</h1>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,16 @@
+import { Fragment } from "react";
+import Header from "../../components/Header";
+import Footer from "../../components/Footer";
+function Home() {
+  return (
+    <Fragment>
+      <Header />
+      <div>
+        <h1>day la home</h1>
+      </div>
+      <Footer />
+    </Fragment>
+  );
+}
+
+export default Home;
