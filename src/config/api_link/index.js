860651d9492b2bce229c5bb43d9c6be225a4e2ba
nhsmai04 <mr.nguyenhandsome04@gmail.com>
2024-11-18 16:32:38 +0700

    lay code vua update 1

--- src/config/api_link/index.js
+++ src/config/api_link/index.js
@@ -1,4 +1,4 @@
 const tours_link = "http://localhost:3000/tours";
 const blogs_link = "http://localhost:3000/blogs";
-
-export { tours_link, blogs_link };
+const destinations_link = "http://localhost:300/destination"
+export { tours_link, blogs_link ,destinations_link};
