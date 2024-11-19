5e5a7613e32031e15a0cf515080890422f96e92e
phu3112004 <22521105@gm.uit.edu.vn>
2024-11-19 10:23:54 +0700

    viet hoa tourdetail

--- src/pages/Tour/TourDetail/TourDetail.js
+++ src/pages/Tour/TourDetail/TourDetail.js
@@ -53,28 +53,28 @@ function TourDetail() {
             <FontAwesomeIcon icon={faStar} className={cx("star-yellow")} />
           </div>
           <p>
-            {data.rate} ({data.reviews} reviews)
+            {data.rate} ({data.reviews} bài đánh giá)
           </p>
         </div>
         <div className={cx("details-info-desc")}>
           <div className={cx("details-info-desc-item")}>
-            <h3>LOCATION</h3>
+            <h3>ĐỊA ĐIỂM</h3>
             <p>{data.location}</p>
           </div>
           <div className={cx("details-info-desc-item")}>
-            <h3>ADVENTURE LEVEL</h3>
+            <h3>MỨC ĐỘ</h3>
             <p>{data.level}</p>
           </div>
           <div className={cx("details-info-desc-item")}>
-            <h3>MINIMUM AGE</h3>
+            <h3>ĐỘ TUỔI TỐI THIỂU</h3>
             <p>{data.age}</p>
           </div>
           <div className={cx("details-info-desc-item")}>
-            <h3>CITY</h3>
+            <h3>THÀNH PHỐ</h3>
             <p>{data.city}</p>
           </div>
           <div className={cx("details-info-desc-item")}>
-            <h3>FROM</h3>
+            <h3>GIÁ TỪ</h3>
             <p>${data.price}</p>
           </div>
         </div>
@@ -127,58 +127,60 @@ function TourDetail() {
       <div className={cx("details-overview")}>
         <div className={cx("details-overview-desc")}>
           <h2>
-            Tuolumne River rafting, available just outside Yosemite National
-            Park, is a standard-setter among all the California whitewater
-            experiences 2PM& offers.
+            Chèo thuyền vượt thác Tuolumne, có sẵn ngay bên ngoài Công viên quốc
+            gia Yosemite, là một tiêu chuẩn trong số tất cả các trải nghiệm vượt
+            thác của California mà NPQ cung cấp.
           </h2>
           <p>
-            Pronounced “too-all-uh-me,” and affectionately referred to as “the
-            T,” it’s one of our “Seven Whitewater Wonders” of the World. But
-            it’s not just the 18 miles of nearly continuous Class IV whitewater
-            that attracts paddlers from all over the world to this Wild & Scenic
-            River. A Tuolumne River rafting trip is a true wilderness adventure
-            in the Yosemite region complete with stunning, scenic beauty,
-            brilliant star-filled nights, and big thrills. And you’ve got
-            options. Choose from an adventurous 48-hour getaway with cool hikes
-            up side canyons carved by cascading waterfalls, or a carefree
-            three-day Tuolumne rafting vacation, with lots of time to explore
-            and relax while reconnecting with nature in a secluded river canyon.{" "}
+            Được phát âm là “too-all-uh-me,” và được gọi trìu mến là “the T,”
+            đây là một trong “Bảy kỳ quan nước trắng” của thế giới. Nhưng không
+            chỉ có 18 dặm nước trắng Cấp IV gần như liên tục thu hút những người
+            chèo thuyền từ khắp nơi trên thế giới đến với Con sông Hoang dã &
+            Phong cảnh này. Chuyến đi bè trên Sông Tuolumne là một cuộc phiêu
+            lưu thực sự giữa thiên nhiên hoang dã ở vùng Yosemite với vẻ đẹp
+            tuyệt vời, những đêm đầy sao rực rỡ và những cảm giác mạnh. Và bạn
+            có nhiều lựa chọn. Lựa chọn giữa chuyến đi nghỉ dưỡng mạo hiểm kéo
+            dài 48 giờ với những chuyến đi bộ đường dài thú vị lên các hẻm núi
+            bên cạnh được tạo thành bởi những thác nước đổ xuống, hoặc kỳ nghỉ
+            chèo bè Tuolumne ba ngày vô tư, với nhiều thời gian để khám phá và
+            thư giãn trong khi kết nối lại với thiên nhiên trong một hẻm núi
+            sông biệt lập.{" "}
           </p>
         </div>
         <div className={cx("details-overview-highlight")}>
-          <h2>Trip Highlights</h2>
+          <h2>Điểm nổi bật của chuyến đi</h2>
           <ul>
             <li>
               <FontAwesomeIcon icon={faCircleCheck} />
-              18 miles of continuous Class IV Tuolumne River whitewater rafting
+              18 dặm đi bè vượt thác liên tục trên sông Tuolumne hạng IV
             </li>
             <li>
               <FontAwesomeIcon icon={faCircleCheck} />
-              Raft 15 unforgettable rapids like Ram’s Head, Clavey Falls, Gray’s
-              Grindstone & more
+              Raft 15 ghềnh thác khó quên như Ram’s Head, Clavey Falls, Gray’s
+              Grindstone & nhiều hơn nữa
             </li>
             <li>
               <FontAwesomeIcon icon={faCircleCheck} />
-              Camp near Yosemite in a Wild & Scenic river canyon
+              Trại gần Yosemite trong hẻm núi sông hoang dã và đẹp như tranh vẽ
             </li>
             <li>
               <FontAwesomeIcon icon={faCircleCheck} />
-              Hike, play bocce ball or horseshoes, swim, explore creeks &
-              waterfalls
+              Đi bộ đường dài, chơi bóng bocce hoặc ném móng ngựa, bơi lội, khám
+              phá các con suối và thác nước
             </li>
             <li>
               <FontAwesomeIcon icon={faCircleCheck} />
-              Wine on the River & Craft Beer Tasting trips available
+              Có sẵn các chuyến đi nếm thử rượu vang trên sông và bia thủ công
             </li>
             <li>
               <FontAwesomeIcon icon={faCircleCheck} />
-              Easy drive from San Francisco, Sacramento & Lake Tahoe
+              Lái xe dễ dàng từ San Francisco, Sacramento và Hồ Tahoe
             </li>
           </ul>
         </div>
       </div>
       <div className={cx("details-itinerary")}>
-        <h1>ITINERARY & MAP</h1>
+        <h1>LỊCH TRÌNH VÀ BẢN ĐỒ</h1>
         <div className={cx("details-itinerary-day")}>
           <div
             className={cx(
@@ -192,13 +194,13 @@ function TourDetail() {
         </div>
         <div className={cx("details-itinerary-info")}>
           <div className={cx("details-itinerary-info-desc")}>
-            <h2>Itinerary at a Glance</h2>
+            <h2>Sơ lược về hành trình</h2>
             <p>
-              We pride ourselves in running a relaxed and flexible schedule.
-              Every Tuolumne River rafting trip is different depending upon the
-              group, other trips on the water, camp locations, and sometimes the
-              weather. The following is a sample of what your trip might be
-              like:
+              Chúng tôi tự hào khi có lịch trình thoải mái và linh hoạt. Mỗi
+              chuyến đi bè trên sông Tuolumne đều khác nhau tùy thuộc vào nhóm,
+              các chuyến đi khác trên mặt nước, địa điểm cắm trại và đôi khi là
+              thời tiết. Sau đây là một số ví dụ về chuyến đi của bạn có thể như
+              thế nào:
             </p>
 
             {Array.from({ length: day }, (_, i) => (
@@ -206,22 +208,22 @@ function TourDetail() {
             ))}
           </div>
           <div className={cx("details-itinerary-info-map")}>
-            <h2>Meeting time & Place</h2>
+            <h2>Thời gian và địa điểm họp</h2>
             <div className={cx("details-itinerary-info-map-item")}>
-              <h3>Location</h3>
+              <h3>Địa điểm</h3>
               <h5>Starbucks</h5>
               <p>3711 E Shaw Ave, Clovis, CA 93619, US</p>
             </div>
             <div className={cx("details-itinerary-info-map-item")}>
-              <h3>Time</h3>
+              <h3>Thời gian</h3>
               <p>8:30 AM</p>
             </div>
             <div className={cx("details-itinerary-info-map-item")}>
-              <h3>Return</h3>
-              <p>Approximately 6:30 PM, depending on water levels</p>
+              <h3>Quay lại</h3>
+              <p>Khoảng 6:30 PM, tùy thuộc vào mực nước</p>
             </div>
             <div className={cx("details-itinerary-info-map-item")}>
-              <h3>Map</h3>
+              <h3>Bản đồ</h3>
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d4974.620366509581!2d-119.64188239823264!3d36.80881264252745!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80945b6798778b57%3A0xe04dbb89bd457e0f!2sStarbucks!5e1!3m2!1svi!2s!4v1719900148105!5m2!1svi!2s"
                 allowfullscreen=""
@@ -230,7 +232,7 @@ function TourDetail() {
             </div>
             <div className={cx("details-itinerary-info-map-item")}>
               <Link to="/contact" className={cx("book-btn")}>
-                BOOK NOW
+                ĐẶT TOUR NGAY
               </Link>
             </div>
           </div>
@@ -238,41 +240,41 @@ function TourDetail() {
       </div>
       <div className={cx("details-review")}>
         <div className={cx("details-review-title")}>
-          <h1>REVIEW SNAPSHOT</h1>
+          <h1>ĐÁNH GIÁ TOUR</h1>
           <span className={cx("details-review-title-rcm")}>
             <h1>100%</h1>
-            <p>of respondents would recommend this to a friend</p>
+            <p>người trả lời sẽ RECOMMEND điều này cho bạn bè</p>
           </span>
         </div>
         <div className={cx("details-review-star")}>
           <div className={cx("details-review-star-destri")}>
-            <h3>RATINGS DISTRIBUTION</h3>
+            <h3>TỶ LỆ ĐÁNH GIÁ</h3>
             <button title="Show only 5 stars reviews">
-              <p>5 Stars</p>
+              <p>5 Sao</p>
               <FontAwesomeIcon icon={faStar} className={cx("star-yellow")} />
               <span className={cx("star-bar", "five-stars")}></span>
               <p>{data.reviews - 8}</p>
             </button>
             <button title="Show only 4 stars reviews">
-              <p>4 Stars</p>
+              <p>4 Sao</p>
               <FontAwesomeIcon icon={faStar} className={cx("star-yellow")} />
               <span className={cx("star-bar", "four-stars")}></span>
               <p>6</p>
             </button>
             <button title="Show only 3 stars reviews">
-              <p>3 Stars</p>
+              <p>3 Sao</p>
               <FontAwesomeIcon icon={faStar} className={cx("star-yellow")} />
               <span className={cx("star-bar", "three-stars")}></span>
               <p>2</p>
             </button>
             <button title="Show only 2 stars reviews">
-              <p>2 Stars</p>
+              <p>2 Sao</p>
               <FontAwesomeIcon icon={faStar} className={cx("star-yellow")} />
               <span className={cx("star-bar", "two-stars")}></span>
               <p>0</p>
             </button>
             <button title="Show only 1 star reviews">
-              <p>1 Star</p>
+              <p>1 Sao</p>
               <FontAwesomeIcon
                 icon={faStar}
                 className={cx("star-yellow", "star-1")}
@@ -290,19 +292,19 @@ function TourDetail() {
               <FontAwesomeIcon icon={faStar} className={cx("star-yellow")} />
             </div>
             <p>
-              <b>{data.rate}</b> ({data.reviews} reviews)
+              <b>{data.rate}</b> ({data.reviews} bài đánh giá)
             </p>
-            <a href="#">Write a review</a>
+            <a href="#">Viết bài đánh giá</a>
           </div>
         </div>
         <div className={cx("details-review-sort")}>
           <select>
-            <option value="">Non-Sort</option>
-            <option value="">Most recent</option>
-            <option value="">Lowest rated</option>
-            <option value="">Highest rated</option>
-            <option value="">Oldest</option>
-            <option value="">Image</option>
+            <option value="">Chọn</option>
+            <option value="">Gần đây nhất</option>
+            <option value="">Đánh giá thấp nhất</option>
+            <option value="">Đánh giá cao nhất</option>
+            <option value="">Cũ nhất</option>
+            <option value="">Có hình ảnh</option>
           </select>
         </div>
         <div className={cx("details-review-area-container")}>
@@ -320,13 +322,13 @@ function TourDetail() {
               <p>{item.content}</p>
               <div className={cx("details-review-area-auth")}>
                 <p>
-                  By <b>{item.auth}</b>
+                  Đăng bởi <b>{item.auth}</b>
                 </p>
                 <p>
-                  At <b>{item.date}</b>
+                  Thời gian <b>{item.date}</b>
                 </p>
                 <p>
-                  From <b>{item.address}</b>
+                  Địa chỉ <b>{item.address}</b>
                 </p>
               </div>
               {item.tags.map((tag, index) => (
@@ -337,7 +339,7 @@ function TourDetail() {
             </div>
           ))}
 
-          <button className={cx("loadmore-btn")}>LOAD MORE</button>
+          <button className={cx("loadmore-btn")}>XEM THÊM</button>
         </div>
       </div>
     </div>
